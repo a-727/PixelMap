290565209ba1a19d8ddd785d921798458eac9c11
a727 <gstoltzfus3@gmail.com>
2024-05-02 17:48:21 -0400

    Added expirimental MonoGame functionality. Probably has bugs. Do not use it. Made DefaultColor private and renamed it to _defaultColor.

--- PixelMap/PixelMap.cs
+++ PixelMap/PixelMap.cs
@@ -1,10 +1,53 @@
-﻿namespace PixelMap
+﻿using System.Diagnostics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PixelMap
 {
     public class PixelMap
     {
         private int[][] _map;
-        public int DefaultColor;
-        readonly private ConsoleColor[] _consoleMap = new ConsoleColor[]{ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.DarkGreen, ConsoleColor.DarkCyan, ConsoleColor.DarkRed, ConsoleColor.DarkMagenta, ConsoleColor.DarkYellow, ConsoleColor.Gray, ConsoleColor.DarkGray, ConsoleColor.Blue, ConsoleColor.Green, ConsoleColor.Cyan, ConsoleColor.Red, ConsoleColor.Magenta, ConsoleColor.Yellow, ConsoleColor.White};
+        private int _defaultColor;
+        private PmGame game;
+        private ConsoleColor[] _consoleMap = new ConsoleColor[] {
+            ConsoleColor.Black, 
+            ConsoleColor.DarkBlue, 
+            ConsoleColor.DarkGreen,
+            ConsoleColor.DarkCyan,
+            ConsoleColor.DarkRed,
+            ConsoleColor.DarkMagenta, 
+            ConsoleColor.DarkYellow, 
+            ConsoleColor.Gray, 
+            ConsoleColor.DarkGray,
+            ConsoleColor.Blue, 
+            ConsoleColor.Green, 
+            ConsoleColor.Cyan, 
+            ConsoleColor.Red, 
+            ConsoleColor.Magenta,
+            ConsoleColor.Yellow, 
+            ConsoleColor.White,
+            ConsoleColor.Red
+        };
+        private Microsoft.Xna.Framework.Color[] _monoMap = new Microsoft.Xna.Framework.Color[] {
+            Microsoft.Xna.Framework.Color.Black,
+            Microsoft.Xna.Framework.Color.DarkBlue,
+            Microsoft.Xna.Framework.Color.DarkGreen,
+            Microsoft.Xna.Framework.Color.DarkCyan,
+            Microsoft.Xna.Framework.Color.DarkRed,
+            Microsoft.Xna.Framework.Color.DarkMagenta,
+            Microsoft.Xna.Framework.Color.DarkGoldenrod,
+            Microsoft.Xna.Framework.Color.Gray,
+            Microsoft.Xna.Framework.Color.DarkGray,
+            Microsoft.Xna.Framework.Color.Blue,
+            Microsoft.Xna.Framework.Color.Green,
+            Microsoft.Xna.Framework.Color.Cyan,
+            Microsoft.Xna.Framework.Color.Red,
+            Microsoft.Xna.Framework.Color.Magenta,
+            Microsoft.Xna.Framework.Color.Yellow,
+            Microsoft.Xna.Framework.Color.White,
+            Microsoft.Xna.Framework.Color.Orange
+        };
         public PixelMap(int defaultColor = 1, int[][]? startingMap = null, int mapSizeX = 100, int mapSizeY = 50)
         {
             if (startingMap is null)
@@ -23,14 +66,21 @@
             {
                 _map = startingMap;
             }
-            DefaultColor = defaultColor;
+            _defaultColor = defaultColor;
         }
+
+        public void SetupMono()
+        {
+            game = new PmGame();
+        }
+        
         public void ResetMap(int resetTo = -1)
         {
             if (resetTo is < 0 or >= 16)
             {
-                resetTo = DefaultColor;
+                resetTo = _defaultColor;
             }
+
             for (var index = 0; index < _map.Length; index++)
             {
                 var t = _map[index];
@@ -40,17 +90,19 @@
                 }
             }
         }
+
         public void SetPixel(int x, int y, int to)
         {
             _map[y][x] = to;
         }
+
         public void SetRectangle(int x, int y, int width, int height, int color)
         {
             for (int i = 0; i < width; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
-                   SetPixel(x+i, y+j, color);
+                    SetPixel(x + i, y + j, color);
                 }
             }
         }
@@ -59,18 +111,21 @@
         {
             if (changeTo < _consoleMap.Length)
             {
-                DefaultColor = changeTo;
+                _defaultColor = changeTo;
             }
         }
         
         public void SetRectangleOutline(int x, int y, int width, int height, int borderSize, int color, int borderColor)
         {
             SetRectangle(x, y, width, height, color); //Main Rectangle
-            SetRectangle(x-borderSize, y-borderSize, width+borderSize*2, borderSize, borderColor); //Top Border + top corners
-            SetRectangle(x-borderSize, y+height, width+borderSize*2, borderSize, borderColor); //Bottom Border + bottom corners
-            SetRectangle(x-borderSize, y, borderSize, height, borderColor); //Left Border
-            SetRectangle(x+width, y, borderSize, height, borderColor);//Right Border
+            SetRectangle(x - borderSize, y - borderSize, width + borderSize * 2, borderSize,
+                borderColor); //Top Border + top corners
+            SetRectangle(x - borderSize, y + height, width + borderSize * 2, borderSize,
+                borderColor); //Bottom Border + bottom corners
+            SetRectangle(x - borderSize, y, borderSize, height, borderColor); //Left Border
+            SetRectangle(x + width, y, borderSize, height, borderColor); //Right Border
         }
+
         private void ConsoleDrawPixel(int color)
         {
             ConsoleColor c_color = _consoleMap[color];
@@ -78,6 +133,7 @@
             Console.BackgroundColor = c_color;
             Console.Write("++");
         }
+
         private void ConsolePixelLine(int[] colors)
         {
             Console.WriteLine();
@@ -86,26 +142,40 @@
                 ConsoleDrawPixel(color);
             }
         }
+
         public void DrawConsole()
         {
             Console.ForegroundColor = ConsoleColor.White;
-            Console.BackgroundColor = _consoleMap[DefaultColor];
+            Console.BackgroundColor = _consoleMap[_defaultColor];
             Console.Clear();
             foreach (int[] line in _map)
             {
                 ConsolePixelLine(line);
             }
         }
+
+        public void DrawMono()
+        {
+            game.DrawFullMap(_map, _defaultColor, _monoMap);
+        }
+
+        public KeyboardState MonoKeyboard()
+        {
+            return game.GetKeyboardState();
+        }
     }
 
     public class PmGameUtilities
     {
-        public static string SelectMenu(string[]? options = null, string prompt = "Please select an option:", ConsoleColor highlightOption = ConsoleColor.Blue) //Use arrow keys to select an option from a menu. Console only.
+        public static string SelectMenu(string[]? options = null, string prompt = "Please select an option:",
+            ConsoleColor highlightOption =
+                ConsoleColor.Blue) //Use arrow keys to select an option from a menu. Console only.
         {
             if (options is null)
             {
                 options = new string[] { "Yes", "No" };
             }
+
             int pos = 0;
             ConsoleColor defaultBackgroundColor = Console.BackgroundColor;
             ConsoleColor defaultForegroundColor = Console.ForegroundColor;
@@ -129,6 +199,7 @@
                         Console.WriteLine($" • {options[i]}");
                     }
                 }
+
                 Console.BackgroundColor = defaultBackgroundColor;
                 Console.WriteLine("Use arrow keys to move. Press space to select.");
                 ConsoleKey currentKey = Console.ReadKey().Key;
@@ -140,6 +211,7 @@
                         {
                             pos = 0;
                         }
+
                         break;
                     case ConsoleKey.UpArrow:
                         pos--;
@@ -147,13 +219,94 @@
                         {
                             pos = options.Length - 1;
                         }
+
                         break;
                     case ConsoleKey.Spacebar:
                         selectedOption = true;
                         break;
                 }
             }
+
             return options[pos];
         }
     }
+
+    public class PmGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private BasicEffect basicEffect;
+        public PmGame(bool showMouse = true)
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = showMouse;
+        }
+        public void DrawRectangle(float x, float y, float width, float height, Microsoft.Xna.Framework.Color color) //x and y are positions for the upper-left hand corner.
+        {
+            VertexPositionColor[] vertices_a = new VertexPositionColor[3];
+            VertexPositionColor[] vertices_b = new VertexPositionColor[3];
+            vertices_a[0] = new VertexPositionColor(new Vector3(x,y, 0), color);
+            vertices_a[1] = new VertexPositionColor(new Vector3(x + width, y, 0), color);
+            vertices_a[2] = new VertexPositionColor(new Vector3(x, y + height, 0), color);
+            vertices_b[0] = new VertexPositionColor(new Vector3(x + width,y +height, 0), color);
+            vertices_b[1] = new VertexPositionColor(new Vector3(x, y + height, 0), color);
+            vertices_b[2] = new VertexPositionColor(new Vector3(x+width, y, 0), color);
+            GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, vertices_a, 0, 1);
+            GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, vertices_b, 0, 1);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+            base.Update(gameTime);
+        }
+
+        public int PixelSize(int x_total, int y_total)
+        {
+            int a = Window.ClientBounds.Width / x_total;
+            int b = Window.ClientBounds.Width / y_total;
+            if (a < b)
+            {
+                return a;
+            }
+            return b;
+        }
+        
+
+        private void DrawMapRow(int[] row, Microsoft.Xna.Framework.Color[] int_to_color, int pixelSize, int y, int x_offset)
+        {
+            for (int i = 0; i < row.Length; i++)
+            {
+                DrawRectangle(x_offset+i*pixelSize, y, pixelSize,pixelSize, int_to_color[row[i]]);
+            }
+        }
+        
+        public void DrawFullMap(int[][] map, int defaultColor, Microsoft.Xna.Framework.Color[] int_to_color)
+        {
+            basicEffect.Projection = Matrix.CreateOrthographicOffCenter(0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height, 0, 0, 1);
+            GraphicsDevice.Clear(int_to_color[defaultColor]);
+            int pixelSize = PixelSize(map[0].Length, map.Length);
+            int x_offset = (int) Math.Floor((double) (Window.ClientBounds.Width-(map[0].Length*pixelSize))/2);
+            int y_offset = (int) Math.Floor((double) (Window.ClientBounds.Height-(map.Length*pixelSize))/2);
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                DrawRectangle(0, 0, Window.ClientBounds.Width, Window.ClientBounds.Height,int_to_color[defaultColor]);
+                for (int i = 0; i < map.Length; i++)
+                {
+                    DrawMapRow(map[i], int_to_color, pixelSize, y_offset+i*pixelSize, x_offset);
+                }
+            }
+        }
+
+        public KeyboardState GetKeyboardState()
+        {
+            return Keyboard.GetState();
+        }
+    }
+    
 }
